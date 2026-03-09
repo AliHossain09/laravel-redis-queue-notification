8cf8aa7a30b0aebd46b108a7ef7d9184fb39bee2
AloHossain09 <alihoossainn@gmail.com>
2026-03-09 12:03:10 +0600

    create scheduler command for daily report

---
+++ app/Console/Commands/DailyReport.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Post;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class DailyReport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:daily-report';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $count = Post::count();
+
+        Log::info('Daily post count: '.$count);
+
+        $this->info('Report Generated');
+    }
+}
